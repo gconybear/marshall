f76db1356a83cfe8400a74c275118201d44cc8b3
gconybear <grantconybear@gmail.com>
2024-03-28 11:39:01 -0600

    code exec output in llms

--- marshall/core/utils.py
+++ marshall/core/utils.py
@@ -1,10 +1,31 @@
 import io 
 import contextlib 
+import random
+import math 
 
-def exec_code(code: str):
-    # Redirect stdout to capture the output of exec
+# def exec_code(code: str): 
+#     namespace = {}
+#     exec(code, namespace)
+
+def exec_code(code):
     with io.StringIO() as buf, contextlib.redirect_stdout(buf):
-        exec(code)
-        # Get the output from the buffer
+        # Include common libraries in the namespace for 'exec'
+        namespace = {
+            "__builtins__": __builtins__,
+            "random": random,
+            "math": math,
+        }
+        exec(code, namespace)
         output = buf.getvalue()
     return output
+
+
+# def exec_code(code: str):
+#     # Redirect stdout to capture the output of exec
+#     with io.StringIO() as buf, contextlib.redirect_stdout(buf):
+#         exec(code)
+#         # Get the output from the buffer
+#         output = buf.getvalue()
+#     return output 
+
+
--- marshall/llms/gpt.py
+++ marshall/llms/gpt.py
@@ -1,5 +1,6 @@
 import requests 
 import json   
+import ast 
 
 # dotenv 
 import os 
@@ -8,10 +9,12 @@ load_dotenv()
 
 # local 
 from marshall.core.llm import LLM
+from marshall.core import utils
+from marshall.prompts import coding_instructions
 
 class GPT(LLM): 
 
-    def __init__(self, model_name: str, config={}, sys_instructions=None) -> None: 
+    def __init__(self, model_name: str, config={}, sys_instructions=None, toolkit=None, **kwargs) -> None: 
 
         super().__init__(model_name, config if config is not None else {}) 
 
@@ -21,13 +24,20 @@ class GPT(LLM):
         self.completion_url = "https://api.openai.com/v1/chat/completions"  
         self.embedding_url = "https://api.openai.com/v1/embeddings"
         self.system_instructions = []    
-
         self.name = 'chatgpt'
 
         if sys_instructions: 
-            self.add_sys_instructions(sys_instructions)
+            self.add_sys_instructions(sys_instructions) 
+
+        self.toolkit = toolkit 
+        if self.toolkit or kwargs.get('code_execute'): 
+            self.json_output = True 
+            self.add_sys_instructions(coding_instructions.INSTRUCTIONS)
+        else: 
+            self.json_output = False
 
     def api_call(self, payload: dict, url: str) -> dict:  
+        
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}"  
@@ -44,17 +54,14 @@ class GPT(LLM):
 
         if mssg: self.system_instructions.append({"user": "system", "content": mssg})
 
-    def generate(self, prompt: str) -> dict:
+    def generate(self, prompt: str, verbose=False) -> dict:
 
         p = {
             "model": self.model_name,
             "messages": self.system_instructions + [{"role": "user", "content": prompt}],
         }   
 
-        if self.config.get('json', False): 
-            # json=True is just for convenience, actual format is added below
-            del self.config['json']   
-
+        if self.json_output: 
             # restricting output type to json 
             self.config.update({"response_format": {'type': 'json_object'}})
 
@@ -65,7 +72,31 @@ class GPT(LLM):
         response = self.api_call(payload=payload, url=self.completion_url) 
         if 'choices' not in response:  
             return None
-            #return {'success': False, 'message': None, 'response': response}  
+            #return {'success': False, 'message': None, 'response': response}   
+        
+        if self.json_output: 
+            # result will be a JSON string, we want to parse it and execute code if necessary
+            if verbose: print('json output')  
+            res_str = response['choices'][0]['message']['content']
+            try: 
+                obj = json.loads(res_str) 
+            except: 
+                try: 
+                    obj = ast.literal_eval(res_str) 
+                except: 
+                    print('error parsing result, looks like: ', res_str) 
+                    return None 
+                
+            if obj.get('content_type') == 'code': 
+                # execute code 
+                if verbose: print('executing: ', obj.get('content'))
+                code_result = utils.exec_code(obj.get('content')) 
+                output_str = f"Executed the following code:\n```python\n{obj.get('content')}```\n\nResult: {code_result}" 
+
+                return output_str  
+            else:  
+                # just a free text response, we can return 
+                return obj.get('content')
         
         return response['choices'][0]['message']['content']
         
